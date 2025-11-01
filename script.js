bbcd347eb0e5a98bb81bd9ab7768e03899c313c6
Votrez-Chin <votrezchin@gmail.com>
2025-11-01 23:36:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ window.addEventListener('DOMContentLoaded', () => {
   document.body.style.transition = 'opacity 1s ease';
   setTimeout(() => (document.body.style.opacity = 1), 100);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const faders = document.querySelectorAll(".fade-in");
+
+  const observerOptions = {
+    threshold: 0.1
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  faders.forEach(fader => {
+    observer.observe(fader);
+  });
+});
